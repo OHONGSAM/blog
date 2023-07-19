f138fc09a822cd336a4dd91dda6e2dc76df21f5f
LeeJiSeop <jjiisub@naver.com>
2023-07-19 15:11:17 +0900

    💄 blog list table column width 조정

--- app/views.py
+++ app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
 from django.views import View
+from blog.models import Post
 
 
 class IndexMain(View):
     def get(self, request):
+        hotposts = Post.objects.order_by('-likes')[:5]
+        print(hotposts)
         context = {
-            'title': 'Main'
+            'posts': hotposts
         }
         return render(request, 'index.html', context)
